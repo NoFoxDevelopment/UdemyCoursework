1fd983e87de508ff9124d1218ec312bf4debf6f2
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2017-12-23 21:12:20 -0500

    Lesson 233

--- Bootcamp/Section24/EJSDemo/app.js
+++ Bootcamp/Section24/EJSDemo/app.js
@@ -10,6 +10,16 @@ app.get("/fallinlovewith/:thing", function (req, res) {
     res.render("love.ejs", {thingVar: thing});
 })
 
+app.get("/posts", function(req, res) {
+    var posts = [
+        {title: "Post 1", author: "NoFoxDevelopment"},
+        {title: "Post 2", author: "NoFoxDevelopment"},
+        {title: "Post 3", author: "NoFoxDevelopment"}
+    ];
+
+    res.render("posts.ejs", {posts: posts});
+})
+
 app.listen(3000, function() {
     console.log("Serving on port 3000")
 })
