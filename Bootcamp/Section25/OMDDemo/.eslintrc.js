8bed07124160aabf8bef1617d7ad982f11213347
NoFox <nofox@nofoxlinux.attlocal.net>
2017-12-27 19:44:14 -0500

    Lecture 245

---
+++ Bootcamp/Section25/OMDDemo/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console":0,
+        "no-undef":0,
+        "no-unused-vars":0,
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
