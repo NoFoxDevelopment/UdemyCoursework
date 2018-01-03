e00fc49c41bdcdf3d78c7e37071f52e632b792db
NoFox <nofox@nofoxlinux.attlocal.net>
2018-01-02 20:59:13 -0500

    RESTful Blog Demo Complete

--- Bootcamp/RESTfulBlogApp/app.js
+++ Bootcamp/RESTfulBlogApp/app.js
@@ -1,13 +1,15 @@
 //App config
-const express		= require("express"),
-		app 		= express(),
-		bodyParser 	= require('body-parser'),
-		mongoose 	= require('mongoose');
+const express			= require("express"),
+		app 			= express(),
+		bodyParser 		= require('body-parser'),
+		mongoose 		= require('mongoose'),
+		methodOverride 	= require('method-override');
 
 mongoose.connect('mongodb://localhost/restfulBlogApp');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 //Mongoose Model Config
 var blogSchema = new mongoose.Schema({
@@ -19,14 +21,57 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model('Blog', blogSchema);
 
-Blog.create({
-	title: 'Test Blog Post',
-	image: 'https://source.unsplash.com/hAsyQkjbPMM',
-	body: 'Cillum qui deserunt et proident pariatur sed tempor occaecat et occaecat aliquip officia aute adipisicing veniam deserunt laboris aliquip occaecat adipisicing duis magna eu veniam ut id dolor duis et magna reprehenderit cillum occaecat cupidatat commodo amet dolor et pariatur et amet cupidatat sed elit aliquip reprehenderit amet dolore reprehenderit reprehenderit sint anim anim sint velit irure aliqua ex id consectetur dolor ex sed reprehenderit aliquip eiusmod tempor et laborum pariatur culpa occaecat irure ut eu qui fugiat et do nostrud ex esse esse anim aliquip in tempor in quis in et aliquip culpa est in et elit fugiat aliquip nostrud minim do nostrud velit amet aute culpa aliqua in reprehenderit laboris proident sint veniam reprehenderit anim sed proident laboris dolore ex magna excepteur nisi minim in elit et est ad minim aliquip sit consectetur nisi sit elit qui sit ex quis amet consequat nulla est voluptate fugiat ut aute labore dolor officia quis ea do proident incididunt commodo ut et commodo anim ea aute consectetur officia aute tempor dolor pariatur quis nostrud ut aute incididunt minim adipisicing aute ut in aliquip in commodo aliqua.'
+app.get('/', function(req, res){
+	res.redirect('/blogs');
 });
-//RESTful routes
 
+//INDEX Route
+app.get('/blogs', function(req, res){
+	Blog.find({}, function(err, blogs) {
+		err ? console.log(err) : res.render('index', {blogs: blogs});
+	});
+});
 
+//NEW Route
+app.get('/blogs/new', function(req, res){
+	res.render('new');
+});
+
+//CREATE Route
+app.post('/blogs', function(req, res){
+	//Create Blog
+	Blog.create(req.body.blog, function(err, newPost){
+		err ? res.render('new') : res.redirect('/blogs');
+	});
+});
+
+//SHOW Route
+app.get('/blogs/:id', function(req, res){
+	Blog.findById(req.params.id, function(err, foundPost){
+		err ? console.log(err) : res.render('show', {blog: foundPost});
+	});
+});
+
+//EDIT Route
+app.get('/blogs/:id/edit', function(req, res){
+	Blog.findById(req.params.id, function(err, foundPost){
+		err ? console.log(err) : res.render('edit', {blog: foundPost});
+	})
+});
+
+//UPDATE Route
+app.put('/blogs/:id', function(req, res) {
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedPost){
+		err ? console.log(err) : res.redirect('/blogs/' + req.params.id);
+	});
+});
+
+//DESTROY Route
+app.delete('/blogs/:id', function(req, res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		err ? console.log(err) : res.redirect('/blogs');
+	});
+});
 
 app.listen(3000, function(){
 	console.log('Being served on port 3000');
