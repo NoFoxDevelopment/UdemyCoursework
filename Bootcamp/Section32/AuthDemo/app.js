af11ba27ce43bbbd7c848791b3d8a93dbac44e73
NoFoxDevelopment <31908496+NoFoxDevelopment@users.noreply.github.com>
2018-02-09 06:18:14 -0500

    minor changes

--- Bootcamp/Section32/AuthDemo/app.js
+++ Bootcamp/Section32/AuthDemo/app.js
@@ -1,23 +1,77 @@
-var express               = require("express"),
-    mongoose              = require("mongoose"),
-    passport              = require("passport"),
-    bodyParser            = require("body-parser"),
-    LocalStrategy         = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose");
+const express               = require('express'),
+			mongoose              = require('mongoose'),
+			passport              = require('passport'),
+			LocalStrategy         = require('passport-local'),
+			passportLocalMongoose = require('passport-local-mongoose'),
+			bodyParser            = require('body-parser'),
+			User                  = require ('./models/user.js');
 
-mongoose.connect("mongodb://localhost/auth-demo-app");
+mongoose.connect('mongodb://localhost/plebianpalace');
 
-var app = express();
+const app = express();
+
 app.set('view engine', 'ejs');
 
+app.use(require('express-session')({
+	secret: "A still more glorious dawn awaits",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//======
+//ROUTES
+//======
+
 app.get('/', function(req, res) {
-  res.render('home');
+	res.render('login');
 });
 
 app.get('/secret', function(req, res) {
-  res.render('secret');
+	res.render('secret');
+});
+
+
+//Auth Routes
+//show signup form
+app.get('/register', function(req, res) {
+	res.render('register');
+});
+
+//user signup handling
+app.post('/register', function(req, res) {
+	req.body.username
+	req.body.password
+	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+		err ? (
+			console.log(err),
+			res.render('register')
+		) : (
+			passport.authenticate('local')(req, res, function(){
+				res.redirect('/secret');
+			})
+		);
+	});
+});
+
+//Login Routes
+//show login form
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+
+app.post('/login', passport.authenticate('local', {
+	successRedirect: '/secret',
+	failureRedirect: '/login'
+}) ,function(req, res) {
 });
 
 app.listen(5000, function() {
-  console.log('Server started...');
+	console.log('app being served on port 5000...');
 });
